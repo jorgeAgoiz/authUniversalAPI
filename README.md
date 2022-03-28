7f8e81547d177fecac2886f00b8c9a9ff6c1927b
jorgeAP <jorgeagoiz@gmail.com>
2022-03-28 22:26:02 +0200

    small details

---
+++ README.md
@@ -0,0 +1,3 @@
+REST API de autenticación universal para todas mis Apps Front-End.
+
+Build with Node & Express & TypeScript
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ app.get("/", (req, res, next) => {
     res.status(200).json({message: "Starting!!"})
 })
 
-
 app.listen(3012, () => {
     console.log("Listen on port 3012...")
 })
