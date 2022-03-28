16c3401ba66216acc5f895605918b1dece368fb1
jorgeAP <jorgeagoiz@gmail.com>
2022-03-28 22:27:32 +0200

    second build

--- build/index.js
+++ build/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Hey There!!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get("/", function (req, res, next) {
+    res.status(200).json({ message: "Starting!!" });
+});
+app.listen(3012, function () {
+    console.log("Listen on port 3012...");
+});
 //# sourceMappingURL=index.js.map
