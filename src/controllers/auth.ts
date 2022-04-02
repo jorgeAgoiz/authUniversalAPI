0fa7a4fda10e16e840b8defe1dffef0e2bc0a288
jorgeAP <jorgeagoiz@gmail.com>
2022-04-02 09:49:42 +0200

    SignUp I think is finished

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,24 +1,47 @@
 import { RequestHandler } from 'express'
-import { User } from '../types/auth'
+import { User, NewUser } from '../types/auth'
 import bcrypt from 'bcrypt'
 import { db } from '../utils/firebaseDb'
-
-const saltRounds = 12
+import { FieldValue } from 'firebase-admin/firestore'
 
 export const signUpUser: RequestHandler = async (req, res) => {
 	const { email, password, confirmPassword }: User = req.body
-	
 	try {
-		if(password !== confirmPassword) {
-			return res.status(404).json({ message: 'Passwords must match' })
+		if (password !== confirmPassword) {
+			return res
+				.status(412)
+				.json({ message: 'Passwords must match' })
 		}
-		const hashedPassword: string = await bcrypt.hash(password, saltRounds)
-		const users = await db.collection('users').get()
-		return res.status(200).json({ result: users.docs[0].data() }) 
+
+		const userExists: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> = 
+			await db.collection('users').where('email', '==', email).get()
+		if (userExists.size > 0) {
+			return res
+				.status(412)
+				.json({ message: 'Email bussy' })
+		}
+
+		const hashedPassword: string = await bcrypt.hash(password, 12)
+		const newUser: NewUser = {
+			email,
+			password: hashedPassword,
+			created_at: FieldValue.serverTimestamp()
+		}
+		const user: FirebaseFirestore.WriteResult = 
+			await db.collection('users').doc().set(newUser)
+		if (!(user.writeTime.seconds > 0)) {
+			return res
+				.status(400)
+				.json({ message: 'Something went wrong' })
+		}
+		return res
+			.status(201)
+			.json({ result: 'User created' }) 
+
 	} catch (error: any) {
-		console.log(error)
+		return res.status(400).json({ message: error.message })
 	}
-}
+}// SIGN UP Finished
 
 
 
@@ -35,6 +58,10 @@ export const rememberPassword: RequestHandler = async (req, res) => {
 	return res.status(200).json({ message: 'Starting!!' })
 }
 
-export const deleteUser: RequestHandler = async (req, res, next) => {
+export const deleteUser: RequestHandler = async (req, res) => {
+	return res.status(200).json({ message: 'Starting!!' })
+}
+
+export const getUserBy: RequestHandler = async (req, res) => {
 	return res.status(200).json({ message: 'Starting!!' })
 }
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,5 +1,13 @@
+import { FieldValue } from 'firebase-admin/firestore'
+
+//SignUp Interfaces
 export interface User {
 	email: string
 	password: string
 	confirmPassword: string
+}
+export interface NewUser {
+	email: string
+	password: string
+	created_at?: FieldValue
 }
